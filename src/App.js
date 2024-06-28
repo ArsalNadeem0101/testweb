03c0d6ce74ce04599f984b5ce7d55a5107481637
adil080891 <97826407+ArsalNadeem0101@users.noreply.github.com>
2024-06-28 20:02:16 +0500

    ..
    //

--- src/App.js
+++ src/App.js
@@ -4,30 +4,26 @@ import { Helmet } from 'react-helmet';
 function App() {
   return (
     <div className="App">
-
       <RecipePage />
-
     </div>
   );
 }
 const RecipePage = ({ recipe }) => {
   return (
     <div>
-      <Helmet>
-        {/* <title>{recipe.title}</title> */}
-        <meta name="description" content={"recipe.description"} />
-        <meta property="og:title" content={"recipe.title"} />
-        <meta property="og:description" content={"recipe.description"} />
-        <meta property="og:image" content={"https://sharecipe.s3.amazonaws.com/17173.png"} />
-        {/* <meta property="og:url" content={`https://example.com/recipes/${recipe.id}`} /> */}
-        <meta property="og:type" content="article" />
-        {/* <h1>{recipe.title}</h1> */}
-      </Helmet>
+      {/* <title>{recipe.title}</title> */}
+      <meta name="description" content={"recipe.description"} />
+      <meta property="og:title" content={"recipe.title"} />
+      <meta property="og:description" content={"recipe.description"} />
+      <meta property="og:image" content={"https://sharecipe.s3.amazonaws.com/17173.png"} />
+      {/* <meta property="og:url" content={`https://example.com/recipes/${recipe.id}`} /> */}
+      <meta property="og:type" content="article" />
+      {/* <h1>{recipe.title}</h1> */}
       <img src={"https://sharecipe.s3.amazonaws.com/17173.png"}
         alt={"recipe.title"}
       />
       {/* <p>{recipe.description}</p> */}
-    </div >
+    </div>
   );
 };
 export default App;
