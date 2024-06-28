4af737374473b05a5624e3e2d0771dfe089ae6fe
adil080891 <97826407+ArsalNadeem0101@users.noreply.github.com>
2024-06-28 19:45:50 +0500

    ..
    ..

--- src/App.js
+++ src/App.js
@@ -13,7 +13,6 @@ function App() {
 const RecipePage = ({ recipe }) => {
   return (
     <div>
-
       <title>{recipe.title}</title>
       <meta name="description" content={"recipe.description"} />
       <meta property="og:title" content={recipe.title} />
@@ -21,7 +20,6 @@ const RecipePage = ({ recipe }) => {
       <meta property="og:image" content={"https://sharecipe.s3.amazonaws.com/17173.png"} />
       <meta property="og:url" content={`https://example.com/recipes/${recipe.id}`} />
       <meta property="og:type" content="article" />
-
       <h1>{recipe.title}</h1>
       <img src={"https://sharecipe.s3.amazonaws.com/17173.png"} alt={recipe.title} />
       <p>{recipe.description}</p>
