7046c4b670ab0a722a1732dd691978748cbc243b
adil080891 <97826407+ArsalNadeem0101@users.noreply.github.com>
2024-06-28 19:03:05 +0500

    updated
    ..

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
 import './App.css';
+import { Helmet } from 'react-helmet';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit...! <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <RecipePage />
     </div>
   );
 }
-
+const RecipePage = ({ recipe }) => {
+  return (
+    <div>
+      <Helmet>
+        <title>{recipe.title}</title>
+        <meta name="description" content={"recipe.description"} />
+        <meta property="og:title" content={recipe.title} />
+        <meta property="og:description" content={recipe.description} />
+        <meta property="og:image" content={"https://sharecipe.s3.amazonaws.com/17173.png"} />
+        <meta property="og:url" content={`https://example.com/recipes/${recipe.id}`} />
+        <meta property="og:type" content="article" />
+      </Helmet>
+      <h1>{recipe.title}</h1>
+      <img src={"https://sharecipe.s3.amazonaws.com/17173.png"} alt={recipe.title} />
+      <p>{recipe.description}</p>
+    </div>
+  );
+};
 export default App;
